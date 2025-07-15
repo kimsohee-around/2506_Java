5714077212f67b057df156d3c87dff333619db0d
kimsohee <koreait.sec2020@gmail.com>
2025-07-15 13:32:29 +0900

    day05 오전

--- partB/src/day05/B23SortTest.java
+++ partB/src/day05/B23SortTest.java
@@ -6,13 +6,14 @@ public class B23SortTest {
   public static void main(String[] args) {
 
     double[] darr1 = new double[10];
-    // 선택정렬 darr1과 같은 값으로 실행할 배열
-    double[] darr2 = darr1.clone(); // darr1 배열을 복사해서 darr2 로 참조
     for (int i = 0; i < darr1.length; i++) {
       darr1[i] = Math.random();
       darr1[i] = Math.floor(darr1[i] * 100) / 100;
       // floor() 메소드로 소수점 이하 2자리 램덤 값 만들기
     }
+    // 선택정렬 darr1과 같은 값으로 실행할 배열
+    double[] darr2 = darr1.clone(); // darr1 배열을 복사해서 darr2 로 참조
+
     System.out.println("랜덤 double 배열 1 : " + Arrays.toString(darr1));
     System.out.println("랜덤 double 배열 2 : " + Arrays.toString(darr2));
 
@@ -22,14 +23,19 @@ public class B23SortTest {
     long endTime = System.nanoTime();
     System.out.println("double 배열 정렬 후: " + Arrays.toString(darr1));
     System.out.println("실행 시간 : " + (endTime - startTime) + " ns");
-    // bubbleSort : 25562200 ns
+    // bubbleSort : 23534700 ns
 
     startTime = System.nanoTime();
     new B23SortTest().selectionSort(darr2);
     endTime = System.nanoTime();
     System.out.println("double 배열 정렬 후: " + Arrays.toString(darr2));
     System.out.println("실행 시간 : " + (endTime - startTime) + " ns");
+    // selectionSort : 2426000 ns
   }
+  // ✅참고 : 정렬(알고리즘)의 성능 측정 -> 시간복잡도. 표기 Big-O. n은 데이터갯수
+  // 버블 정렬, 선택정렬 : O(n의 제곱)
+  // 병합 정렬, 퀵 정렬... : O(n log n) < O(n의 제곱)
+  // Arrays.sort() 등 라이브러리는 성능이 좋은 알고리즘 사용합니다.
 
   void selectionSort(double[] arr) {
     for (int i = 0; i < arr.length - 1; i++) {
@@ -41,7 +47,7 @@ public class B23SortTest {
           arr[j] = temp;
         }
       }
-      System.out.println(String.format("i=%d  %s", i, Arrays.toString(arr)));
+      // System.out.println(String.format("i=%d %s", i, Arrays.toString(arr)));
     }
   }
 
@@ -56,7 +62,7 @@ public class B23SortTest {
           arr[j + 1] = temp;
         }
       }
-      System.out.println(String.format("i=%d  %s", i, Arrays.toString(arr)));
+      // System.out.println(String.format("i=%d %s", i, Arrays.toString(arr)));
     }
   }
 }
