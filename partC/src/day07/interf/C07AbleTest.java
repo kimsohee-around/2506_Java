062d3d79a723b705b36aff3e91c2e6d59c12992e
kimsohee <koreait.sec2020@gmail.com>
2025-07-17 13:41:35 +0900

    day07 오전

--- partC/src/day07/interf/C07AbleTest.java
+++ partC/src/day07/interf/C07AbleTest.java
@@ -1,7 +1,7 @@
 package day07.interf;
 
 // 다형성
-//   ㄴ 인터페이스 구현 클래스는 인터페이스로 대입
+//   ㄴ 인터페이스 구현 클래스는 해당 인터페이스 타입으로 대입
 //   ㄴ 메소드의 인자 또는 리턴값의 타입이 됩니다.
 public class C07AbleTest {
 
@@ -10,6 +10,15 @@ public class C07AbleTest {
     // tomorrow(new AI()); // 오류
     someday(new AI("GPT"));
     tomorrow(new Dog());
+    // 인터페이스 구현하는 익명 클래스 정의
+    Cookable robot = new Cookable() {
+      @Override
+      public void cook(String material) {
+        System.out.println(material + " 인스턴트 요리를 합니다.");
+      }
+    };
+    today(robot);
+
   }
 
   private static void today(Cookable cookable) {
