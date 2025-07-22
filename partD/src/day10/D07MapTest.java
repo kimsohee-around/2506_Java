9c4efe313854b241697621c6fe68c5e169eb02e1
kimsohee <koreait.sec2020@gmail.com>
2025-07-22 13:39:46 +0900

    day10 오전

---
+++ partD/src/day10/D07MapTest.java
@@ -0,0 +1,20 @@
+package day10;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class D07MapTest {
+  public static void main(String[] args) {
+    Map<String, String> map = new HashMap<>();
+    // 1 . 데이터 저장
+    map.put("t1", "사나");
+    map.put("t2", "모모");
+    map.put("t3", "나연");
+    map.put("t4", "다현");
+    map.put("t1", "나나");
+
+    // 2.
+
+  }
+
+}
