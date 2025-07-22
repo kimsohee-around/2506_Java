1792bf709d5c46f0c8c44c8c2c1c80ba2d2b4c8e
kimsohee <koreait.sec2020@gmail.com>
2025-07-22 10:21:38 +0900

    day10 오전

--- partD/src/day09/D05MemberManagerApp.java
+++ partD/src/day09/D05MemberManagerApp.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class D05MemberManagerApp {
   public static void main(String[] args) {
     List<Member> list = new ArrayList<>();
+    // list 초기화 메소드 실행
+    initMemberList(list); // list 변수 참조값 전달
     System.out.println("회원 관리 프로그램입니다.");
     boolean status = true;
     while (status) {
@@ -19,16 +21,25 @@ public class D05MemberManagerApp {
           break;
         case "R":
           int index = Integer.parseInt(
-              System.console().readLine("삭제할 인덱스 입력 >>>>"));
-          // System.out.println(index);
+              System.console().readLine("\t삭제할 인덱스 입력 >>>>"));
+          list.remove(index);
           break;
         case "U":
           index = Integer.parseInt(
-              System.console().readLine("수정할 인덱스 입력 >>>>"));
-          // System.out.println(index);
+              System.console().readLine("\t수정할 인덱스 입력 >>>>"));
+          double point = Double.parseDouble(System.console().readLine("\t포인트 입력 >>> "));
+          if (confirm("포인트 변경")) {
+            list.get(index).setPoint(point);
+            // ✅ index 의 객체를 가져와서 (Member 타입) setter 로 point 값 변경
+            System.out.println("\t업데이트 결과 : " + list.get(index));
+          } else {
+            System.out.println("\t포인트 변경을 취소했습니다.");
+          }
           break;
         case "S":
-
+          System.out.println("\t\t회원 리스트");
+          for (int i = 0; i < list.size(); i++)
+            System.out.println("\t" + i + ":  " + list.get(i));
           break;
         case "E":
           status = false;
@@ -38,6 +49,19 @@ public class D05MemberManagerApp {
           break;
       }
     }
+  }
+
+  // 프로그램 실행할 때 마다 4개의 객체를 리스트에 저장하고 시작 : list 초기화
+  // 메소드 인자는 main 메소드의 ArrayList 객체의 주소를 저장.(객체의 주소를 공유)
+  private static void initMemberList(List<Member> list) {
+    Member m1 = new Member(1, "김땡떙", 133.4);
+    Member m2 = new Member(2, "반하나", 111.9);
+    Member m3 = new Member(3, "최사과", 85.4);
+    Member m4 = new Member(4, "박모모", 96.3);
+
+    // 불변 객체 리스트가 메소드의 인자로 모든 요소를 가변 객체 list 에 추가
+    list.addAll(List.of(m1, m2, m3, m4));
+
   }
 
   private static void addMember(List<Member> list) {
