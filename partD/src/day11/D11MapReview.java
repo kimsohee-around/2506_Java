7811db4c2b11cc85bfde71c8e668244eec36b47b
kimsohee <koreait.sec2020@gmail.com>
2025-07-23 15:53:22 +0900

    day11 오후

--- partD/src/day11/D11MapReview.java
+++ partD/src/day11/D11MapReview.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 //Map 복습 : 활요예시 일회성으로 데이터를 저장하고 전달하는 목적
-//        클래스 정의를 대신할 수 있습니다.
+//        클래스 정의를 대신할 수 있습니다. => 자바스크립트의 객체 형식과 유사합니다.
 public class D11MapReview {
 
   public static void main(String[] args) {
@@ -18,14 +18,29 @@ public class D11MapReview {
     MapTest test = new MapTest();
     test.setMember(map);
     System.out.println("test : " + test);
+
     // 퀴즈 : test 객체에 저장된 member 를 가져와서. id, name, point 를 출력해보세요.(각각)
-    // hint : Object 타입을 직접 출력해보고. casting 하여 출력도 해보세요.
+    // hint : a) Object 타입을 직접 출력해보고. b) casting 하여 출력도 해보세요.
     // 1. getter 로 Map타입 member 가져오기
-
+    Map<String, Object> member = test.getMember();
     // 2. 1번 Map타입 member 에서 key 값 모두 가져오기
+    System.out.println("keySet : " + member.keySet()); // [id, name, point]
+    // a) toString 출력은 Object 타입에서 실제 데이터의 타입으로 자동 다운 캐스팅
+    System.out.println(member.get("id")); // 1000 (Object타입이 Integer 타입으로 다운)
+    System.out.println(member.get("name")); // "김사나" (Object타입이 String 타입으로 다운)
+    System.out.println(member.get("point")); // 3.14 (Object타입이 Double 타입으로 다운)
 
-    // 3. 2번의 keySet 으로 반복문 (key-value 출력)
+    // 3. (key-value 출력)
+    // b) 변수에 저장하려면 직접 강제 캐스팅
+    int id2 = (int) member.get("id");
+    String name2 = (String) member.get("name");
+    double point2 = (double) member.get("point");
+    System.out.println("id=" + id2);
+    System.out.println("name=" + name2);
+    System.out.println("point=" + point2);
 
+
+    
   }
 }
 
